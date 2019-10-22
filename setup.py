8f3cfb697438cc617651923ff951d6cbb2757edf
SpinaNico <spinanico93@gmail.com>
2019-10-22 09:48:22 +0200

    add | setup release 0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mockstore',
+    version='0.0.1',
+    description='mock complete of google cloud firestore version: 1.0.0',
+    author='Spina Nico',
+    author_email='spinanico93@gmail.com',
+    url='https://github.com/SpinaNico/python-mockstore',
+    packages=["mockstore"],
+    install_requires=[]
+)
